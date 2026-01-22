078f8c3e75e371d517395463c31d37053f8a907b
Facundo Diaz <dfacundo905@gmail.com>
2026-01-22 16:35:27 -0300

    se creo la DB local y configuraciones

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import express from "express";
+import { connectDB } from "./src/config/config.db.js";
 const app = express();
 const PORT = 3000;
+
+//middleware para leer json
+app.use(express.json())
+
+//conexion base de datos
+connectDB();
 
 app.listen(PORT, () => {
   console.log("Servidor corriendo en el puerto:", PORT);
 });
 
-app.get("/", (req, res)=>{
-    res.status(200).json({mensaje:"su consulta salio bien"})
-})
+app.get("/", (req, res) => {
+  res.status(200).json({ mensaje: "su consulta salio bien" });
+});
