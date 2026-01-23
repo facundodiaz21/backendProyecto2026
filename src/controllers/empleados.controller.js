e571b322e5e8c065397c780018c0ac9a50e46f3c
Facundo Diaz <dfacundo905@gmail.com>
2026-01-23 16:11:12 -0300

    se creo rutas, controlador y servicios

--- src/controllers/empleados.controller.js
+++ src/controllers/empleados.controller.js
@@ -1,4 +1,8 @@
-import { crearEmpleadoService, obtenerEmpleadosService } from "../services/empleados.service.js";
+import {
+  crearEmpleadoService,
+  obtenerEmpleadosPorIdService,
+  obtenerEmpleadosService,
+} from "../services/empleados.service.js";
 
 export const obtenerEmpleadosController = async (req, res) => {
   const empleados = await obtenerEmpleadosService();
@@ -11,6 +15,20 @@ export const crearEmpleadoController = async (req, res) => {
   const empleado = await crearEmpleadoService(req.body);
   res.status(201).json({
     mensaje: "Empleado creado con exito!",
+    empleado,
+  });
+};
+export const obtenerEmpleadosPorIdController = async (req, res) => {
+  const id = req.params.id;
+  const empleado = await obtenerEmpleadosPorIdService(id);
+
+  if (!empleado)
+    return res.status(404).json({
+      mensaje: "usuario no encontrado",
+      datos: null,
+    });
+  return res.status(202).json({
+    mensaje: "usuario encontrado con exito",
     empleado,
   });
 };
--- src/routes/empleados.routes.js
+++ src/routes/empleados.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { crearEmpleadoController, obtenerEmpleadosController } from "../controllers/empleados.controller.js";
+import { crearEmpleadoController, obtenerEmpleadosController, obtenerEmpleadosPorIdController } from "../controllers/empleados.controller.js";
 const router = Router();
 
 router.get("/", obtenerEmpleadosController);
 router.post("/registro", crearEmpleadoController);
+router.get("/:id", obtenerEmpleadosPorIdController)
 export default router;
