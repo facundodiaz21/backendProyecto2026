0e8aaf8bf35b5bf6e6846b5960e56d12bc2141b9
Facundo Diaz <dfacundo905@gmail.com>
2026-01-23 15:41:10 -0300

    se modifico el servicio de empleado

--- src/services/empleados.service.js
+++ src/services/empleados.service.js
@@ -1,8 +1,10 @@
 import { empleadoModel } from "../models/empleados.models.js";
 
-export const obtenerEmpleadosService = async ()=>{
-    return await empleadoModel.find()
-}
-export const crearEmpleadoService = async (data)=>{
-    return await empleadoModel.create(data)
-}
+export const obtenerEmpleadosService = async () => {
+  return await empleadoModel.find({
+    estado: { $ne: "inactivo" },
+  });
+};
+export const crearEmpleadoService = async (data) => {
+  return await empleadoModel.create(data);
+};
