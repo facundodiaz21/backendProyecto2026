3fc5aa2b40474af40bccaf38f8ea0394fc453b06
Facundo Diaz <dfacundo905@gmail.com>
2026-01-23 15:14:15 -0300

    se creo rutas, controlador y servicios

--- src/routes/empleados.routes.js
+++ src/routes/empleados.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { obtenerEmpleadosController } from "../controllers/empleados.controller.js";
+import { crearEmpleadoController, obtenerEmpleadosController } from "../controllers/empleados.controller.js";
 const router = Router();
 
 router.get("/", obtenerEmpleadosController);
+router.post("/registro", crearEmpleadoController);
 export default router;
--- src/services/empleados.service.js
+++ src/services/empleados.service.js
@@ -3,3 +3,6 @@ import { empleadoModel } from "../models/empleados.models.js";
 export const obtenerEmpleadosService = async ()=>{
     return await empleadoModel.find()
 }
+export const crearEmpleadoService = async (data)=>{
+    return await empleadoModel.create(data)
+}
