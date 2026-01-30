37c278fb2b1f7f6484f9d9988db3bb6e6916ffe6
Facundo Diaz <dfacundo905@gmail.com>
2026-01-30 14:55:38 -0300

    middleware de errores globales

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { connectDB } from "./src/config/config.db.js";
 import routes from "./src/routes/index.routes.js"
+import { errorHandle } from "./src/middleware/errorMiddleware.js";
 const app = express();
 const PORT = 3000;
 
@@ -9,7 +10,7 @@ app.use(express.json())
 app.use("/api", routes)
 //conexion base de datos
 connectDB();
-
+app.use(errorHandle);
 app.listen(PORT, () => {
   console.log("Servidor corriendo en el puerto:", PORT);
 });
