86ee178ca1c58bf231edae9ffbf211c1965b4d36
JuliSantillan02 <santillanjuli2002@gmail.com>
2026-01-31 13:36:27 -0300

    se realizo hasheo de contraseña, ajustes en servicios y controladores

--- src/services/usuarios.service.js
+++ src/services/usuarios.service.js
@@ -1,19 +1,21 @@
 import UsuarioModel from "../models/usuarios.model.js"
-
+import argon from "argon2"
 
 export const registroUsuarioServicio = async (datosUsuario)=>{
-    try {
-       const nuevoUsuarioDB = new UsuarioModel (datosUsuario);
-       await nuevoUsuarioDB.save();
-       console.log(nuevoUsuarioDB);
-       return {
-      statusCode: 201,
-      json: { msg: "Usuario Creado", usuarioCreado: nuevoUsuarioDB },
-    };
-  } catch (error) {
-    return {
-      statusCode: 400,
-      json: { msg: "Error al crear usuario", usuarioCreado: null },
-    };
-    }
+  const nuevoUsuarioDB = new UsuarioModel (datosUsuario);
+  nuevoUsuarioDB.password = await argon.hash(nuevoUsuarioDB.password);
+  await nuevoUsuarioDB.save();
+  return nuevoUsuarioDB;
 }
+
+export const loginUsuarioServicio = async (datosUsuario) => {
+  const usuarioExistente = await UsuarioModel.findOne({
+    usuario: datosUsuario.usuario,
+  });
+  if(!usuarioExistente) return null
+    
+ const contraseniaOk = await argon.verify(usuarioExistente.password,datosUsuario.password);
+    if (!contraseniaOk) return null;
+
+  return usuarioExistente
+  }
