95fb333e3371a57586518dbed80b9ad6745fc13c
Facundo Diaz <dfacundo905@gmail.com>
2026-01-31 15:50:32 -0300

    correcion de rutas e instlacion de cors

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import express from "express";
 import { connectDB } from "./src/config/config.db.js";
-import routes from "./src/routes/index.routes.js"
+import routes from "./src/routes/index.routes.js";
 import { errorHandle } from "./src/middleware/errorMiddleware.js";
+import cors from "cors";
 
 const app = express();
 const PORT = 3000;
 
 //middleware para leer json
-app.use(express.json())
-app.use("/api", routes)
+app.use(cors());
+app.use(express.json());
+app.use("/api", routes);
 //conexion base de datos
 connectDB();
 app.use(errorHandle);
